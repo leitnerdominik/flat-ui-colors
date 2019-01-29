c22db9e6ca3abafb2421a1fc9dbb7b1476adbfb8
Dominik Leitner <leitner.dominik@hotmail.de>
2019-01-29 21:41:51 +0100

    v0.1

---
+++ docs/precache-manifest.85be6596ddbeb9bfc52695a76328b0d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f496e2cd5b0ecb39fbc",
+    "url": "/static/css/main.b9b22e55.chunk.css"
+  },
+  {
+    "revision": "4f496e2cd5b0ecb39fbc",
+    "url": "/static/js/main.4f496e2c.chunk.js"
+  },
+  {
+    "revision": "e545d94a9bc4563603d1",
+    "url": "/static/js/1.e545d94a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3b3ccc5c26caf00f4644c10d6c92ce8",
+    "url": "/static/media/alert.d3b3ccc5.mp3"
+  },
+  {
+    "revision": "f95ae7fba254751805669d04e8512a89",
+    "url": "/index.html"
+  }
+];
